fe7dea7c0870bced0e9715d1c627033ae65a1298
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-08-26 13:42:25 -0700

    add working properly on calculator lab

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // import functions
 
-// reference needed DOM elements
+import { add } from './calculator.js';
 
+// reference needed DOM elements
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
 // set event listeners 
+addButton.addEventListener('click', () => {
+    const value1 = addNumber1.value;
+    const value2 = addNumber2.value;
+    const result = add(value1, value2);
+    addAnswer.textContent = result;
+});
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,22 @@
 // IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { add } from '../calculator.js';
 
 const test = QUnit.test;
 
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const number1 = 7;
+    const number2 = 3;
+
+    const expected = 10;
     
-    //Act 
+    //Act
+    const result = add(number1, number2); 
+
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = result; // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
