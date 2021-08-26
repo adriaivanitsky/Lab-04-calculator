8fea5d8807650b35d48efaead21309f1de27d607
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-08-26 15:10:00 -0700

    all calculator functions added, plus a little css to make it look better

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import functions
 
-import { add, subtract } from './calculator.js';
+import { add, subtract, multiply, divide } from './calculator.js';
 
 // reference needed DOM elements
 //addition calculator
@@ -15,7 +15,21 @@ const subtractNumber2 = document.getElementById('input2subtract');
 const subtractButton = document.getElementById('enter-button-subtract');
 const subtractAnswer = document.getElementById('difference');
 
+//multiplication calculator
+const multiplyNumber1 = document.getElementById('input1multiply');
+const multiplyNumber2 = document.getElementById('input2multiply');
+const multiplyButton = document.getElementById('enter-button-multiply');
+const multiplyAnswer = document.getElementById('product');
+
+//division calculator
+const divideNumber1 = document.getElementById('input1divide');
+const divideNumber2 = document.getElementById('input2divide');
+const divideButton = document.getElementById('enter-button-divide');
+const divideAnswer = document.getElementById('quotient');
+
 // set event listeners 
+
+//addition
 addButton.addEventListener('click', () => {
     const value1 = addNumber1.value;
     const value2 = addNumber2.value;
@@ -23,12 +37,33 @@ addButton.addEventListener('click', () => {
     addAnswer.textContent = result;
 });
 
+//subtraction
 subtractButton.addEventListener('click', () => {
     const value1 = subtractNumber1.value;
     const value2 = subtractNumber2.value;
     const result = subtract(value1, value2);
     subtractAnswer.textContent = result;
-})
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+
+});
+
+//multiplication
+multiplyButton.addEventListener('click', () => {
+    const value1 = multiplyNumber1.value;
+    const value2 = multiplyNumber2.value;
+    const result = multiply(value1, value2);
+    multiplyAnswer.textContent = result;
+    
+});
+
+//division
+divideButton.addEventListener('click', () => {
+    const value1 = divideNumber1.value;
+    const value2 = divideNumber2.value;
+    const result = divide(value1, value2);
+    divideAnswer.textContent = result;
+
+});
+
+// get user input(s)
+// do any needed work with the value(s)
+// update DOM to reflect new value(s)
--- calculator.js
+++ calculator.js
@@ -5,3 +5,11 @@ export function add(n1, n2) {
 export function subtract(n1, n2) {
     return Number(n1) - Number(n2);
 }
+
+export function multiply(n1, n2) {
+    return Number(n1) * Number(n2);
+}
+
+export function divide(n1, n2) {
+    return Number(n1) / Number(n2);
+}
