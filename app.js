bb3907f3c1063aab9d5b90038c912068c983b9cc
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-08-26 13:53:15 -0700

    edited html to makeid's make more sense to me

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 import { add } from './calculator.js';
 
 // reference needed DOM elements
-const addNumber1 = document.getElementById('add-number-1');
-const addNumber2 = document.getElementById('add-number-2');
-const addButton = document.getElementById('add-button');
-const addAnswer = document.getElementById('add-answer');
+const addNumber1 = document.getElementById('input1');
+const addNumber2 = document.getElementById('input2');
+const addButton = document.getElementById('enter-button');
+const addAnswer = document.getElementById('sum');
 
 // set event listeners 
 addButton.addEventListener('click', () => {
     const value1 = addNumber1.value;
     const value2 = addNumber2.value;
+
     const result = add(value1, value2);
     addAnswer.textContent = result;
 });
